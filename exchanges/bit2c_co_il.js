5d6469eb1bfc840f0291ffc8cc95c1613c2a376e
Natan <natan.shalva@gmail.com>
2017-12-19 16:12:30 +0200

    v-2

---
+++ exchanges/bit2c_co_il.js
@@ -0,0 +1,72 @@
+
+var gg =  function() {
+var bit2c = require('bit2c');
+
+
+
+/*
+bit2c.getOrderBook('BtgNis', function(error, ticker) {
+    console.log('-------------------------------------------');
+    console.log('-               BT2C      - Btg           -');
+    console.log('-------------------------------------------');
+    console.log(ticker);
+    console.log('');
+  //  return ticker;
+  //  BUY__BTG__IN_BI2C =  ticker.ll ;
+  // console.log(' buy BTG price in nis:' , ticker.h );
+  //  console.log(' buy BTG price in nis - after fee :' , ticker.h * 0.995   );
+  });
+  */
+
+
+bit2c.getTicker('BtgNis', function(error, ticker) {
+  console.log('-------------------------------------------');
+  console.log('-               BT2C      - Btg           -');
+  console.log('-------------------------------------------');
+  console.log(ticker);
+  console.log('');
+  //  return ticker;
+  //   ticker.ll ;
+  // console.log(' buy BTG price in nis:' , ticker.h );
+  console.log(' buy BTG price in nis - after fee :' , ticker.h * 0.995   );
+
+/*  bit2c.getTicker('BtcNis', function(error, bitcoin) {
+    console.log(bitcoin);
+    BUY__BTG__IN_BI2C = ( ticker.l * 0.995)  /  bitcoin.h;
+    console.log( 'BUY__BTG__IN_BI2C',BUY__BTG__IN_BI2C ) ;
+    SELL__BTG__IN_BI2C = ( ticker.h * 0.995)  /  bitcoin.h;
+    console.log( 'SELL__BTG__IN_BI2C',SELL__BTG__IN_BI2C ) ;
+
+    BUY__BTG__IN_BIT_Z_COM____SELL__BTG__IN_BI2C  = BUY__BTG__IN_BIT_Z_COM  - SELL__BTG__IN_BI2C;
+
+    BUY__BTG__IN_BI2C__SELL__BTG__IN_BIT_Z_COM = BUY__BTG__IN_BI2C - SELL__BTG__IN_BIT_Z_COM ;
+
+    var read_number_in_nis = parseFloat(BUY__BTG__IN_BI2C__SELL__BTG__IN_BIT_Z_COM * bitcoin.h).toFixed(2)  ;
+    console.log( 'buy in bi2c and sell in bit-z.com - in NIS' ,  read_number_in_nis );
+
+    console.log( 'buy in bit-z.com and sell in bit-z.com - in NIS' , BUY__BTG__IN_BIT_Z_COM____SELL__BTG__IN_BI2C * bitcoin.h);
+  });*/
+
+});
+
+
+
+
+
+/*
+
+  credentials = {
+    key: '4176aad7-abca-4d1d-b8cc-3f0285860c15',
+    secret: '9DB3266F66425611A3A0BE622C07C1402D31753F7C5D27FCA2E5E30F4F7710FD'
+  };
+
+// getting your current balance
+  bit2c.getBalance(credentials, function(error, balance) {
+    console.log('-------------------------------------------');
+    console.log('-               BT2C                      -');
+    console.log('-------------------------------------------');
+    console.log(balance);
+  });*/
+};
+
+module.exports  =  gg();
---
+++ exchanges/bit_z_com.js
@@ -0,0 +1,70 @@
+//var prettyjson = require('prettyjson');
+
+// var request = require('request');
+var rp = require('request-promise');
+
+console.log('-------------------------------------------');
+console.log('-               bit-z.com                 -');
+console.log('-               btg_btc                   -');
+console.log('-------------------------------------------');
+
+  var gg =  function() {
+
+    var options = {
+      uri: 'https://www.bit-z.com/api_v1/ticker?coin=btg_btc',
+      qs: {
+        access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
+      },
+      headers: {
+        'User-Agent': 'Request-Promise'
+      },
+      json: true // Automatically parses the JSON string in the response
+    };
+
+    rp(options)
+    .then(function (da) {
+    DEBUG && console.log(da);
+    })
+    .catch(function (err) {
+      // API call failed...
+    });
+    
+/*    request(url, function(error, response, body) {
+      const objectBody = JSON.parse(body);
+      const util = require('util');
+
+      var niceBody = util.inspect(objectBody, {depth: null, colors: true});
+
+      var dataToPrint = {
+        url: url,
+        error: error,
+        statusCode: response && response.statusCode,
+        body: niceBody
+      };
+
+      var options = {
+        noColor: false
+      };
+
+      console.log(prettyjson.render(dataToPrint, options));
+
+      //https://www.bit-z.com/about/fee
+      // trade fee 0.1%
+      //withdrow = 0.5%
+
+  //    var bit_z_com = {};
+
+/!*        bit_z_com.buy__BTG = (objectBody.data.sell * 0.999 ) * 0.995;
+        console.log(' buy BTG in bit-z.com price in BTC - after fee :',
+            bit_z_com.buy__BTG);
+        bit_z_com.sell__BTG = (objectBody.data.buy * 0.999 ) * 0.995;
+        console.log('SEll -  BTG in bit-z.com price in BTC - after fee :',
+            bit_z_com.sell__BTG);*!/
+    });*/
+
+};
+
+module.exports  =  gg();
+
+
+
