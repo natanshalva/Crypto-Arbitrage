1c23e6c64284c5938ae38325b90df3b63a2d6168
Natan <natan.shalva@gmail.com>
2017-12-30 17:35:38 +0200

    v-42

--- ltc_btc/bit2c_co_il__bit_z_com.js
+++ ltc_btc/bit2c_co_il__bit_z_com.js
@@ -326,16 +326,14 @@ function run_in_loop_wrapper() {           //  create a loop function
           DEBUG && console.log('buy_in_Bit_z_com__sell_in_Bi2c: '.info, price_margin);
 
 
-
           var params_of_examine_to_store = require('./custom_moduls/prper_for_sending')(coin_name,pair_coin, exchange_a, exchange_b, price_margin,helper_functions);
           require('./custom_moduls/store_data.js')(params_of_examine_to_store, STORE_NEGATIVE_RESOLES);
           //------------------------------------------------------------------------------------
 
-
           var price_margin = exchange_b.u_can_sell_in_Bit_z_com - exchange_a.u_can_buy_in_BI2C_for_BTC;
           DEBUG && console.log('buy_in_BI2C_sell_in_BIT_Z_COM'.info, price_margin);
 
-          var params_of_examine_to_store = require('./custom_moduls/prper_for_sending')(coin_name,pair_coin, exchange_b  , exchange_a,  price_margin,helper_functions);
+          var params_of_examine_to_store = require('./custom_moduls/prper_for_sending')(coin_name,pair_coin, exchange_b  , exchange_a, price_margin,helper_functions);
           require('./custom_moduls/store_data.js')(params_of_examine_to_store, STORE_NEGATIVE_RESOLES);
 
         };
