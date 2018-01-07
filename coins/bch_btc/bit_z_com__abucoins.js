790b460adf39937e3003393a9f5818571fe10345
Natan <natan.shalva@gmail.com>
2018-01-07 16:08:07 +0200

    v-58

--- coins/bch_btc/bit_z_com__abucoins.js
+++ coins/bch_btc/bit_z_com__abucoins.js
@@ -1,5 +1,5 @@
- DEBUG = false;
-// DEBUG = true;
+// DEBUG = false;
+ DEBUG = true;
 
 console.log('Arber is running ...');
 
@@ -10,7 +10,7 @@ var pair_coin = 'BTC' ;
 var counting_rounds = 0 ;
 // var STORE_NEGATIVE_RESOLES = true ;
 
-var delay_in_milliseconds = 5000;
+var DELAY_TIME = 5000;
 
 var r = require('../../common/all_require');
 //DEBUG && console.log(r);
@@ -28,82 +28,10 @@ function run_in_loop_wrapper() {           //  create a loop function
 
   r.async.parallel([
         function get__bit_z_com(callback) {
-          var rp = require('request-promise');
-          var options = {
-            //  uri: 'https://www.bit-z.com/api_v1/ticker?coin=btg_btc',
-            uri: 'https://www.bit-z.com/api_v1/depth?coin=bch_btc', qs: {
-              //   access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
-            }, headers: {
-              'User-Agent': 'Request-Promise',
-            }, json: true // Automatically parses the JSON string in the response
-          };
-          rp(options).then(function(da) {
-            //  DEBUG && console.log(da);
-            callback(null, da);
-          }).catch(function(err) {
-            // API call failed...
-          });
+          require('../../exchanges/get_exchange_data/bit_z_com.js')(callback,'bch_btc');
         }, function(callback) {
 
-          const rp = require('request-promise');
-          const crypto = require('crypto');
-
-          const TIMEOUT = 5000;
-
-          class Abucoins {
-            constructor() {
-              this.endpoint = 'https://api.abucoins.com';
-              this.credentials = {
-                key: '10513875-XZAEVTFUOK87G6192P3NYMDWSB05CHRJ',
-                secret: 'LzRSRE13SnJXcCV0SV0xS0YpfTc9NXghWUIwKkUiPnw2PzwsT2BffkAmaFU5UUNn',
-                passphrase: 'ij5tgkjdb',
-              }
-            }
-
-            _jsonRequest(options) {
-              options.timeout = TIMEOUT;
-              options.json = true;
-
-              return rp(options);
-            }
-
-            _getHeaders(sign, timestamp) {
-              return {
-                'AC-ACCESS-KEY': this.credentials.key,
-                'AC-ACCESS-SIGN': sign,
-                'AC-ACCESS-TIMESTAMP': timestamp,
-                'AC-ACCESS-PASSPHRASE': this.credentials.passphrase,
-              };
-            }
-
-            signAndRequest(method, path, body = {}) {
-              const timestamp = Math.floor(new Date() / 1000);
-              let options = {
-                uri: `${this.endpoint}${path}`, method: method, body: body,
-              };
-              let string = timestamp + method + path;
-              if (Object.keys(body).length > 0) {
-                string += JSON.stringify(body);
-              }
-              const sign = crypto.createHmac('sha256',
-                  Buffer.from(this.credentials.secret, 'base64')).
-                  update(string).
-                  digest('base64');
-              options.headers = this._getHeaders(sign, timestamp);
-
-              return this._jsonRequest(options);
-            }
-          }
-
-          let abucoins = new Abucoins();
-          let orders = abucoins.signAndRequest('GET', `/products/BCH-BTC/book?level=1`);
-
-          orders.then((list) => {
-            // console.log(list);
-            callback(null, list );
-
-          });
-
+          require('../../exchanges/get_exchange_data/abucoins_com.js')(callback, , 'bch_btc');
 
         },
       ], // optional callback
@@ -167,7 +95,7 @@ function run_in_loop_wrapper() {           //  create a loop function
 
       }); // end
 
-  helper_functions.loop_function(run_in_loop_wrapper,delay_in_milliseconds);
+  helper_functions.loop_function(run_in_loop_wrapper,DELAY_TIME);
 
 };
 
