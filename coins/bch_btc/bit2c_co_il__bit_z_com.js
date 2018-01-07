ae137974c882d1b5b405255d6dd631de5405ce7f
Natan <natan.shalva@gmail.com>
2018-01-07 16:01:21 +0200

    v-57

--- coins/bch_btc/bit2c_co_il__bit_z_com.js
+++ coins/bch_btc/bit2c_co_il__bit_z_com.js
@@ -30,7 +30,7 @@ function run_in_loop_wrapper() {           //  create a loop function
 
   r.async.parallel([
         function get__bit_z_com(callback) {
-          require('../../exchanges/get_exchange_data/bit_z_com.js')(callback);
+          require('../../exchanges/get_exchange_data/bit_z_com.js')(callback,'bch_btc');
         }, function(callback) {
           //  callback(null,1);
           bit2c.getOrderBook('BchNis', function(error, getOrderBook) {
--- exchanges/get_exchange_data/bit_z_com.js
+++ exchanges/get_exchange_data/bit_z_com.js
@@ -1,10 +1,10 @@
-module.exports = function get_data(callback) {
+module.exports = function get_data(callback , coins) {
   DEBUG && console.log('we are in get_data - bit_z_com.js');
 
   var rp = require('request-promise');
   var options = {
     //  uri: 'https://www.bit-z.com/api_v1/ticker?coin=btg_btc',
-    uri: 'https://www.bit-z.com/api_v1/depth?coin=bch_btc', qs: {
+    uri: 'https://www.bit-z.com/api_v1/depth?coin='+coins, qs: {
       //   access_token: 'xxxxx xxxxx' // -> uri + '?access_token=xxxxx%20xxxxx'
     }, headers: {
       'User-Agent': 'Request-Promise',
